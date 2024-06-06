ed20d4cf3979c8e907b89a1fdf271e0aa932d4b1
Siddhesh Gawade <siddheshgawade61@gmail.com>
2024-06-06 12:19:58 +0530

    removing unncecessay code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import CountryCard from './CountryCard';
 
 function App() {
   return (
     <div className="App">
-      {/* <div>app</div> */}
       <CountryCard/>
     </div>
   );
